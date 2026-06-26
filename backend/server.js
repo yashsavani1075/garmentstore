3474739d583dc08d4de3fa7287dc5411b90d3102
Yash Savani <yashsavani1075@gmail.com>
2026-06-26 12:04:45 +0530

    add vercel url to cors

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "https://garmentstore.vercel.app",
     "http://127.0.0.1:5173"
   ],
   credentials: true
